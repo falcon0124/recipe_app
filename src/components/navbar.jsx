5525d0055909efe1ad7d82233f422beecb9cfbff
sangam saurav <sangamsh04@gmail.com>
2025-02-14 02:09:41 +0530

    adding hamburger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,8 @@
+import React,{ useState } from "react"
+
 export default function Navbar(){
+    const [showSidebar, setShowSidebar] = useState(false)
+
     return(
         <div className="navbar container">
             <a href="#!" className="logo">F<span>oo</span>diesHub</a>
@@ -6,6 +10,11 @@ export default function Navbar(){
                 <a href="#!">Home</a>
                 <a href="#!">Recipes</a>
                 <a href="#!">settings</a>
+            </div>
+            <div className={showSidebar ? "sidebar-btn active" : "sidebar-btn"} onClick={() => {setShowSidebar(!showSidebar)}}>
+                <div className="bar"></div>      
+                <div className="bar"></div>      
+                <div className="bar"></div>      
             </div>
         </div>
     )
